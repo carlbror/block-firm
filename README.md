248edebd016a7b14e9eb55b7d29196b09b083231
carlbror <carl.bror@gmail.com>
2019-02-08 15:44:59 +0100

    Added vote-for-president functionality

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This is built on [TokenERC20](https://theethereum.wiki/w/index.php/ERC20_Token_S
 
 It has only a few functions. The president of the company can publish annual reports and appoint accountants. The accountant for the annual report can verify it, or not, and every accountant can publish problems with the management of the company.
 
-As of yet, the president can not be elected by the shareholders.
+Now the president can be elected by the shareholders.
 
 There is a setup and tutorial on how to use the contract at [Stocks and Bucket](https://stocksandbucket.com/company-interface).
--- blockfirm.sol
+++ blockfirm.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.25;
 
 contract StockCompany{
   struct OtherId{
@@ -44,8 +44,11 @@ contract StockCompany{
   OtherId[] public otherIds;
 
   mapping (address => uint256) public balanceOf;
+  mapping (address => address) public nomineeOf;
+  mapping (address => uint256) public votesFor;
 
   event Transfer(address indexed from, address indexed to, uint256 value);
+  event Vote(address indexed voter, address indexed nominee, int256 votes);
 
   constructor(uint256 _capitalStock, string _name, string _symbol, address _president, string _constitution, uint256 _accountingMonth) public{
       capitalStock = _capitalStock;
@@ -62,7 +65,24 @@ contract StockCompany{
     require(msg.sender == president);
     otherIds.push(OtherId(_name, _id, _system));
   }
-  function changeAccountant(address _accountant) public{
+  function voteForPresident(address _nominee) public{
+    require(balanceOf[msg.sender] > 0);
+    require(nomineeOf[msg.sender] != _nominee);
+
+    if(nomineeOf[msg.sender] != address(0)){
+      votesFor[nomineeOf[msg.sender]] -= balanceOf[msg.sender];
+      emit Vote(msg.sender, nomineeOf[msg.sender], -int(balanceOf[msg.sender]));
+    }
+
+    nomineeOf[msg.sender] = _nominee;
+    votesFor[_nominee] += balanceOf[msg.sender];
+
+    emit Vote(msg.sender, _nominee, int(balanceOf[msg.sender]));
+
+    if(votesFor[_nominee] > (capitalStock/2)+1)
+      president = _nominee;
+  }
+  function addAccountant(address _accountant) public{
     require(msg.sender == president);
     accountants.push(Accountant(_accountant, now));
   }
@@ -106,6 +126,15 @@ contract StockCompany{
     emit Transfer(_from, _to, _value);
 
     assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+
+    if(nomineeOf[_from] != address(0)){
+      votesFor[nomineeOf[_from]] -= _value;
+      emit Vote(_from, nomineeOf[_from], -int(_value));
+    }
+    if(nomineeOf[_to] != address(0)){
+      votesFor[nomineeOf[_to]] += _value;
+      emit Vote(_to, nomineeOf[_to], int(_value));
+    }
   }
   function transfer(address _to, uint256 _value) public {
       _transfer(msg.sender, _to, _value);
